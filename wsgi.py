3954c8f72f0bf40e914c634a1477d6dd92d1f4f9
Sihan Tawsik <sihantawsik@gmail.com>
2020-05-10 19:11:06 +0600

    updated wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,1 @@
-import os
-
-from src import app
-
-if __name__ == ("__main__"):
-    PORT = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=PORT)
+from src import app as application
