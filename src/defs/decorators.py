0356a08502d116cd6a19506f2066d64e51c65ba4
sihan-tawsik <sihantawsik@gmail.com>
2020-05-13 22:38:59 +0600

    added decorator

---
+++ src/defs/decorators.py
@@ -0,0 +1,33 @@
+from flask import request, make_response, jsonify
+
+from src.models import User
+
+def login_required(function):
+    def wrap(*args, **kwargs):
+        auth_header = request.headers.get("Authorization")
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    "status": "fail",
+                    "message": "Bearer token malformed.",
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = None
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                return function(*args, **kwargs)
+            responseObject = {"status": "fail", "message": resp}
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                "status": "fail",
+                "message": "Provide a valid auth token.",
+            }
+            return make_response(jsonify(responseObject)), 401
+
+    wrap.__name__ = function.__name__
+    return wrap
