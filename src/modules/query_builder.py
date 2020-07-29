a970046fe79d83ac43e82dfae05cee3de244e1ec
sihan-tawsik <sihantawsik@gmail.com>
2020-07-29 14:19:23 +0600

    added query class methods

--- src/modules/query_builder.py
+++ src/modules/query_builder.py
@@ -1,2 +1,10 @@
+from src import db
+
 class QueryBuilder:
-    pass
+    def __init__(self, query_class: tuple, query_args: list):
+        self.query = db.session.query(query_class)
+        for element in query_args:
+            self.query.filter(element[0].__dict__[element[1]] == element[2])
+
+    def execute(self):
+        return self.query.all()
