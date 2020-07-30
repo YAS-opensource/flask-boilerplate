7868bcdc9408fb79ab2c1b174f1aa2071a31b09d
sihan-tawsik <sihantawsik@gmail.com>
2020-07-30 12:40:27 +0600

    added query entities

--- src/modules/query_builder.py
+++ src/modules/query_builder.py
@@ -1,10 +1,14 @@
 from src import db
 
+
 class QueryBuilder:
-    def __init__(self, query_class: tuple, query_args: list):
+    def __init__(self, query_class: tuple, query_args: list, query_entities=None):
         self.query = db.session.query(query_class)
         for element in query_args:
-            self.query.filter(element[0].__dict__[element[1]] == element[2])
+            self.query = self.query.filter(
+                element[0].__dict__[element[1]] == element[2]
+            )
+        self.query = self.query.with_entities(query_entities)
 
     def execute(self):
         return self.query.all()
